7f3b4f860439f4601a8ef6a7261f72d34b408456
Trishna Paswan <S24CSEU0083@bennett.edu.in>
2025-07-01 22:41:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for, session, flash, jsonify
+from flask import Flask, render_template, request, redirect, url_for, session, flash, jsonify 
 import csv 
 import os
 
