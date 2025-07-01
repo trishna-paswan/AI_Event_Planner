6b5b68b427c43a7e3f525c88072301a1f37f4f04
Trishna Paswan <S24CSEU0083@bennett.edu.in>
2025-07-01 22:43:35 +0530

    Update mail_utils.py

--- mail_utils.py
+++ mail_utils.py
@@ -6,7 +6,7 @@ def send_email(to_email, event_name):
     msg['Subject'] = f"Event Reminder: {event_name}"
     msg['From'] = 'your_email@gmail.com'
     msg['To'] = to_email
-
+ 
     try:
         with smtplib.SMTP_SSL('smtp.gmail.com', 465) as server:
             server.login("your_email@gmail.com", "your_app_password")
